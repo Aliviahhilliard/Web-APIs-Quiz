fb8a864302c6bf255eeaf6c51de54818eab7e8f2
Aliviah Hilliard <131189770+Aliviahhilliard@users.noreply.github.com>
2023-05-27 23:45:14 -0400

    adding files

---
+++ questions.js
@@ -0,0 +1,29 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Which keyword is used to declare a variable in JavaScript?',
+    choices: ['var', 'let', 'const', 'int'],
+    answer: 'var',
+  },
+  {
+    title: 'Which CSS property is used to control the spacing between elements?',
+    choices: ['margin', 'padding', 'border', 'float'],
+    answer: 'margin',
+  },
+  {
+    title: 'What does the "typeof" operator return for a string value?',
+    choices: ['string', 'number', 'boolean', 'object'],
+    answer: 'string',
+  },
+  {
+    title: 'Which operator is used to assign a value to a variable in JavaScript',
+    choices: ['=', '==', '===', ':='],
+    answer: '=',
+  },
+  {
+    title: 'Which operator is used for concatenating two strings in JavaScript',
+    choices: ['+', '-', '*', '/'],
+    answer: '+',
+  },
+];
+
